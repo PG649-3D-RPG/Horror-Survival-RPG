9cfbc71138c4e87afa24e1fdee9787104cdef4c8
Markus Muegge <markus.muegge@t-online.de>
2022-11-01 18:32:56 +0100

    Implemented first person character controller

---
+++ Assets/Input/PlayerInputState.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputState : MonoBehaviour
+{
+	[Header("Character Input Values")]
+	public Vector2 Move;
+	public Vector2 Look;
+	public bool Jump;
+	public bool Sprint;
+
+	[Header("Movement Settings")]
+	public bool AnalogMovement;
+
+	[Header("Mouse Cursor Settings")]
+	public bool CursorLocked = true;
+	public bool CursorInputForLook = true;
+
+	public void OnMove(InputValue value)
+	{
+		Move = value.Get<Vector2>();
+	}
+	
+	public void OnLook(InputValue value)
+	{
+		if (CursorInputForLook)
+		{
+			Look = value.Get<Vector2>();
+		}
+	}
+
+	public void OnJump(InputValue value)
+	{
+		Jump = value.isPressed;
+	}
+
+	public void OnSprint(InputValue value)
+	{
+		Sprint = value.isPressed;
+	}
+
+	private void OnApplicationFocus(bool hasFocus)
+	{
+		Cursor.lockState = CursorLocked ? CursorLockMode.Locked : CursorLockMode.None;
+	}
+}
